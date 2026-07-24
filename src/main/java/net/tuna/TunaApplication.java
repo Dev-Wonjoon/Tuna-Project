db9d9308e806ceac25f7df3caaaad0af7fa0e43b
doohee kim <doo3721@gmail.com>
2026-07-24 11:48:52 +0900

    INIT: 프로젝트 생성
    - 기초 Spring Boot 프로젝트 생성
    - github PR 자동 규칙 생성
    - README.md 생성

---
+++ src/main/java/net/tuna/TunaApplication.java
@@ -0,0 +1,13 @@
+package net.tuna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TunaApplication.class, args);
+	}
+
+}
