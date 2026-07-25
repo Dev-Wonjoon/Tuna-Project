b5baea45fba97319984d54502a88b12349f3dcdd
Dev-Wonjoon <dnjswns775@gmail.com>
2026-07-26 03:22:00 +0900

    FEAT: post-detail.html 추가 및 detail controller 추가

--- src/main/java/net/tuna/mock/MockHomeController.java
+++ src/main/java/net/tuna/mock/MockHomeController.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Map;
@@ -55,5 +56,46 @@ public class MockHomeController {
         model.addAttribute("currentPlaylistId", null);
 
         return "pages/home";
+    }
+
+    @GetMapping("/posts/{postId}")
+    public String detail(
+            @PathVariable long postId,
+            Model model
+    ) {
+        Map<String, Object> post = jdbcTemplate.queryForMap("""
+            SELECT
+                p.id,
+                p.title,
+                p.content,
+                p.view_count AS viewCount,
+                p.created_at AS createdAt,
+                m.id AS authorId,
+                m.email AS authorEmail
+            FROM posts p
+            JOIN members m
+                ON m.id = p.member_id
+            WHERE p.id = ?
+        """, postId);
+        List<Map<String, Object>> comments = jdbcTemplate.queryForList("""
+            SELECT
+                c.id,
+                c.content,
+                c.member_id AS member_id,
+                c.post_id AS postId,
+                c.created_at AS createdAt,
+                c.updated_at AS updatedAt,
+                m.email AS authorEmail
+            FROM comments c
+            JOIN members m
+                ON m.id = c.member_id
+            WHERE c.post_id = ?
+            ORDER BY c.created_at ASC, c.id ASC
+        """, postId);
+
+        model.addAttribute("title", post.get("title"));
+        model.addAttribute("post", post);
+        model.addAttribute("comments", comments);
+        return "pages/post-detail";
     }
 }
