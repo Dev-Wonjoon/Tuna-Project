2f874d6a5f31eb25208c1a11a71b5c82f59d874c
leesuhwan <acmquwjm@gmail.com>
2026-07-30 10:24:44 +0900

    FEAT: 로그아웃 기능 추가

---
+++ src/main/resources/static/js/logout.js
@@ -0,0 +1,11 @@
+function openLogoutModal() {
+    const modal = document.getElementById("logoutModal");
+    modal.classList.remove("hidden");
+    modal.classList.add("flex");
+}
+
+function closeLogoutModal() {
+    const modal = document.getElementById("logoutModal");
+    modal.classList.remove("flex");
+    modal.classList.add("hidden");
+}
