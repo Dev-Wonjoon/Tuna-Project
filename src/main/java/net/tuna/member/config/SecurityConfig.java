2f180a20f097e9d85a9be344f1f211185c3471c4
leesuhwan <acmquwjm@gmail.com>
2026-07-31 16:37:52 +0900

    FEAT: 로그인 로그아웃시 팝업창 구현

--- src/main/java/net/tuna/member/config/SecurityConfig.java
+++ src/main/java/net/tuna/member/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
                         .loginProcessingUrl("/login")
                         .usernameParameter("email") // input name="email"
                         .passwordParameter("password")
-                        .defaultSuccessUrl("/", true)
+                        .defaultSuccessUrl("/?login", true)
                         .failureUrl("/login?error")
                         .permitAll()
                 )
