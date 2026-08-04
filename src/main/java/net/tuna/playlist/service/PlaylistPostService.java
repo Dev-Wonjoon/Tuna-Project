13858f21d98ce43fff35a11e3c76d898522124c5
Dev-Wonjoon <dnjswns775@gmail.com>
2026-08-04 14:07:56 +0900

    FEAT: 음악 썸네일 및 상세 임베드 적용 - 음악 URL 기반 플레이리스트 곡 썸네일 생성

--- src/main/java/net/tuna/playlist/service/PlaylistPostService.java
+++ src/main/java/net/tuna/playlist/service/PlaylistPostService.java
@@ -27,6 +27,7 @@ public class PlaylistPostService {
     private final PlaylistPostRepository playlistPostRepository;
     private final MusicThumbnailResolver musicThumbnailResolver;
     private final CursorCodec cursorCodec;
+    private final MusicThumbnailResolver musicThumbnailResolver;
 
     public PlaylistPostService(
             PlaylistMusicUrlChecker playlistMusicUrlChecker, PlaylistPostRepository playlistPostRepository,
@@ -37,6 +38,7 @@ public class PlaylistPostService {
         this.playlistPostRepository = playlistPostRepository;
         this.musicThumbnailResolver = musicThumbnailResolver;
         this.cursorCodec = cursorCodec;
+        this.musicThumbnailResolver = musicThumbnailResolver;
     }
 
     public List<PostDto> getPosts(long playlistId, long memberId) {
