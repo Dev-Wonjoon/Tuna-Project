25da5d0209a54504e3b7f8bc9687951b9bc03438
Dev-Wonjoon <dnjswns775@gmail.com>
2026-08-05 15:29:18 +0900

    FIX: 플레이리스트 길이 제한

--- src/main/java/net/tuna/playlist/dto/Playlist.java
+++ src/main/java/net/tuna/playlist/dto/Playlist.java
@@ -22,7 +22,7 @@ public class Playlist {
 
     @Setter
     @NotBlank(message = "이 필드는 필수입니다.")
-    @Size(min = 1, max = 255, message = "최소 1자에서 최대 255자 사이입니다.")
+    @Size(min = 1, max = 30, message = "최소 1자에서 최대 30자 사이입니다.")
     @Pattern(regexp = "^[A-Za-z가-힣0-9 ]+$", message = "한글, 영문, 숫자만 사용할 수 있습니다.")
     private String name;
 
